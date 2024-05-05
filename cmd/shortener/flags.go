efff7a9842fac5b7794517ab4879957575a346fd
RIBorisov <borissoffr@yandex.ru>
2024-05-05 21:46:46 +0300

    fix?

--- cmd/shortener/flags.go
+++ cmd/shortener/flags.go
@@ -8,7 +8,7 @@ var flagRunAddr string
 var flagRunBaseAddr string
 
 func parseFlags() {
-	flag.StringVar(&flagRunAddr, "a", "", "address and port to run server, example: localhost:8080")
+	flag.StringVar(&flagRunAddr, "a", "localhost:8080", "address and port to run server, example: localhost:8080")
 	flag.StringVar(&flagRunBaseAddr, "b", "http://localhost:8080", "server address")
 	flag.Parse()
 }

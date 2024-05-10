95d93af6478604206aee911efd6e0d84b78c3505
RIBorisov <borissoffr@yandex.ru>
2024-05-10 20:28:55 +0300

    flags refactor

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -7,18 +7,15 @@ import (
 type Flags struct {
 	RunAddr     string
 	RunBaseAddr string
-	Parsed      bool
 }
 
-func ParseFlags() Flags {
-	f := Flags{}
-	fs := flag.NewFlagSet("flags", flag.ExitOnError)
-	fs.StringVar(&f.RunAddr, "a", "localhost:8080", "address and port to run server, example: localhost:8080")
-	fs.StringVar(&f.RunBaseAddr, "b", "http://localhost:8080", "server address")
-	fsParsed := fs.Parsed()
-	if !fsParsed {
-		_ = fs.Parse([]string{})
-		f.Parsed = true
+var f Flags
+
+func parseFlags() *Flags {
+	if !flag.Parsed() {
+		flag.StringVar(&f.RunAddr, "a", "localhost:8080", "address and port to run server, example: localhost:8080")
+		flag.StringVar(&f.RunBaseAddr, "b", "http://localhost:8080", "server address")
+		flag.Parse()
 	}
-	return f
+	return &f
 }

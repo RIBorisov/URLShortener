9796f2526063a26bf099e5e0109ff6084cbe2cd4
RIBorisov <borissoffr@yandex.ru>
2024-05-22 15:21:55 +0300

    check logs

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -1,8 +1,10 @@
 package middleware
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
+	"reflect"
 	"time"
 
 	"github.com/go-chi/chi/v5/middleware"
@@ -12,20 +14,20 @@ func New(log *slog.Logger) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
 			start := time.Now()
-
-			log.Info(
-				"incoming request",
-				slog.String("method", r.Method),
-				slog.String("path", r.URL.Path),
-				slog.String("duration", time.Since(start).String()),
-			)
 			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+			fmt.Printf("\n>>>r.Headers %+v<<<", r.Header.Clone())
+			fmt.Printf("\n>>>r.URL %+v<<<", r.URL)
+			fmt.Printf("\n>>>TypeOf(r.URL.Path) %+v<<<", reflect.TypeOf(r.URL.Path))
+			fmt.Printf("\n>>>r.URL.Path %+v<<<\n", r.URL.Path)
 
 			defer func() {
 				log.Info(
-					"request completed",
+					"OK",
+					slog.String("method", r.Method),
+					slog.String("path", r.URL.Path),
 					slog.Int("status", ww.Status()),
 					slog.Int("size", ww.BytesWritten()),
+					slog.String("duration", time.Since(start).String()),
 				)
 			}()
 			next.ServeHTTP(ww, r)

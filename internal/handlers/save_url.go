bb655f3f759d4020f775b901293e758457812c0c
RIBorisov <borissoffr@yandex.ru>
2024-05-22 22:22:34 +0300

    check & undo

--- internal/handlers/save_url.go
+++ internal/handlers/save_url.go
@@ -1,10 +1,13 @@
 package handlers
 
 import (
+	"bytes"
+	"compress/gzip"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"shortener/internal/logger"
 	"shortener/internal/service"
@@ -12,7 +15,30 @@ import (
 
 func SaveHandler(svc *service.Service) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		long, err := io.ReadAll(r.Body)
+		var body io.Reader = r.Body
+		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+			// если в запросе пришел заголовок Content-Encoding: gzip, пробуем декомпрессить
+			// создаем Reader
+			gr, err := gzip.NewReader(r.Body)
+			if err != nil {
+				logger.Err("failed to create gzip reader", err)
+				http.Error(w, "", http.StatusInternalServerError)
+				return
+			}
+			defer gr.Close()
+
+			// создаем приемщик прочитанного декомпресса
+			var buf bytes.Buffer
+			_, err = io.Copy(&buf, gr)
+			if err != nil {
+				logger.Err("failed to decompress gzip body", err)
+				http.Error(w, "", http.StatusInternalServerError)
+				return
+			}
+			body = &buf
+		}
+
+		long, err := io.ReadAll(body)
 		if err != nil {
 			logger.Err("failed to read body", err)
 			http.Error(w, "", http.StatusInternalServerError)

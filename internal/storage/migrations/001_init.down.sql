35f5dc41a5a788db48aed752bc7fde862b0a678d
RIBorisov <borissoffr@yandex.ru>
2024-07-02 14:33:47 +0300

    minor: fix migrations count

--- internal/storage/migrations/001_init.down.sql
+++ internal/storage/migrations/001_init.down.sql
@@ -1,4 +1,13 @@
 BEGIN TRANSACTION ;
+
+DROP INDEX IF EXISTS idx_short_is_not_deleted;
+
+ALTER TABLE urls
+DROP COLUMN is_deleted,
+DROP COLUMN user_id;
+
+DROP INDEX IF EXISTS idx_long_url;
+
 DROP TABLE urls;
 
 COMMIT;
--- internal/storage/migrations/003_user_id_and_idx.down.sql
+++
@@ -1,9 +0,0 @@
-BEGIN TRANSACTION;
-
-DROP INDEX IF EXISTS idx_short_is_not_deleted;
-
-ALTER TABLE urls
-DROP COLUMN is_deleted,
-DROP COLUMN user_id;
-
-COMMIT;
